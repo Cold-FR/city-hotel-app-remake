866fd73d9e9db8b5b7e30282ecc7b7aee9dacc2d
Cold <51854565+Cold-FR@users.noreply.github.com>
2023-11-13 21:12:07 +0100

    Gestion des liens + Accélération matérielle

--- app/app.js
+++ app/app.js
@@ -11,7 +11,9 @@ const deltaUpdater = new DeltaUpdater({
 });
 
 let win = null;
-let checkUpdate = null;
+let appStarted = false;
+
+app.commandLine.appendSwitch('enable-hardware-overlays');
 
 const createWindow = () => {
     win = new BrowserWindow({
@@ -29,6 +31,29 @@ const createWindow = () => {
     win.loadFile(path.join(__dirname, 'index.html')).then(() => {
         win.maximize();
         win.webContents.openDevTools();
+
+        win.webContents.setWindowOpenHandler(({url}) => {
+            const urlFormat = new URL(url);
+            console.log(urlFormat.href);
+
+            if (!urlFormat.hostname.includes('habbocity.me') && !urlFormat.hostname.includes('funados-radio.fr') && urlFormat.href !== 'about:blank') {
+                require('electron').shell.openExternal(urlFormat.href);
+            } else if (urlFormat.pathname === '/discord') {
+                require('electron').shell.openExternal('https://discord.gg/EDtGr4Cr7V');
+            } else {
+                return {
+                    action: 'allow',
+                    overrideBrowserWindowOptions: {
+                        autoHideMenuBar: true,
+                        backgroundColor: "#9569f3",
+                        parent: win,
+                        icon: path.join(__dirname, '/icon.png')
+                    }
+                }
+            }
+
+            return {action: 'deny'};
+        })
     });
 };
 
@@ -89,27 +114,29 @@ app.on('window-all-closed', () => {
 
 /// UPDATER
 deltaUpdater.on('download-progress', (d) => {
-    function formatBytes(bytes, decimals = 2) {
-        if (bytes === 0) return '0 Bytes';
+    if (appStarted) {
+        function formatBytes(bytes, decimals = 2) {
+            if (bytes === 0) return '0 Bytes';
 
-        const k = 1024;
-        const dm = decimals < 0 ? 0 : decimals;
-        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+            const k = 1024;
+            const dm = decimals < 0 ? 0 : decimals;
+            const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-        const i = Math.floor(Math.log(bytes) / Math.log(k));
+            const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+            return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+        }
+
+        win.setProgressBar(0);
+        win.send('downloadProgress', {
+            speed: formatBytes(d.bytesPerSecond),
+            percent: d.percent,
+            transferred: formatBytes(d.transferred),
+            total: formatBytes(d.total)
+            //inBack: appStart
+        });
+        win.setProgressBar(d.percent / 100);
     }
-
-    win.setProgressBar(0);
-    win.send('downloadProgress', {
-        speed: formatBytes(d.bytesPerSecond),
-        percent: d.percent,
-        transferred: formatBytes(d.transferred),
-        total: formatBytes(d.total)
-        //inBack: appStart
-    });
-    win.setProgressBar(d.percent / 100);
 });
 
 contextMenu({
