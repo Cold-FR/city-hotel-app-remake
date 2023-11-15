52d6ba6376891103b0550444af8659df07f09749
Cold <51854565+Cold-FR@users.noreply.github.com>
2023-11-15 20:51:15 +0100

    Github Actions

--- .electron-delta.js
+++ .electron-delta.js
@@ -18,10 +18,7 @@ const options = {
 };
 
 exports.default = async function (context) {
-    console.log('\n\n\n');
-    console.log(Array.from(context.platformToTargets)[0][0].name);
-    console.log('\n\n\n');
-    if(Array.from(context.platformToTargets)[0].name === 'mac') return;
+    if(Array.from(context.platformToTargets)[0][0].name === 'mac') return;
     
     const deltaInstallerFiles = await DeltaBuilder.build({
         context,
