b586978567f5e024fa99e486007f5cc27ff3416d
Cold <51854565+Cold-FR@users.noreply.github.com>
2023-11-19 20:33:22 +0100

    Gestion du zoom

--- app/preload.js
+++ app/preload.js
@@ -10,4 +10,18 @@ contextBridge.exposeInMainWorld('electronAPI', {
 
 window.addEventListener('keydown', (e) => {
     if(e.key === 'F11') ipcRenderer.send('fullscreen');
+    if((e.metaKey || e.ctrlKey) && e.key === '+') ipcRenderer.send('zoomIn');
+    if((e.metaKey || e.ctrlKey) && e.key === '-') ipcRenderer.send('zoomOut');
+    if((e.metaKey || e.ctrlKey) && e.key === '=') ipcRenderer.send('zoomReset');
+    if(e.key === 'F5' || ((e.metaKey || e.ctrlKey) && (e.key === 'R' || e.key === 'r'))) ipcRenderer.send('reloadView');
+});
+
+window.addEventListener('wheel', (e) => {
+   if(e.ctrlKey || e.metaKey) {
+       if (e.deltaY < 0) {
+           ipcRenderer.send('zoomIn');
+       } else {
+           ipcRenderer.send('zoomOut');
+       }
+   }
 });
