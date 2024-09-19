4f5b2ec053853be0eb94983ecfed2d98044de601
Cold-FR <51854565+Cold-FR@users.noreply.github.com>
2024-09-19 14:08:44 +0200

    Delete unused code

--- app/preload.js
+++ app/preload.js
@@ -1,13 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI', {
-    /*clearCache: () => ipcRenderer.send('clearCache'),
-    zoomIn: () => ipcRenderer.send('zoomIn'),
-    zoomReset: () => ipcRenderer.send('zoomReset'),
-    zoomOut: () => ipcRenderer.send('zoomOut'),
-    reloadView: () => ipcRenderer.send('reloadView')*/
-});
-
 window.addEventListener('keydown', (e) => {
     if(e.key === 'F11') ipcRenderer.send('fullscreen');
     if((e.metaKey || e.ctrlKey) && e.key === '+') ipcRenderer.send('zoomIn');
