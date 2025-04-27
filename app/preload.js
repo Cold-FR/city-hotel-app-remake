ba93366b39e1d56e161663cc15f141166a86b907
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-04-27 20:48:44 +0200

    Cleanup

--- app/preload.js
+++ app/preload.js
@@ -1,19 +1,19 @@
-const { ipcRenderer } = require('electron');
+const {ipcRenderer} = require('electron');
 
 window.addEventListener('keydown', (e) => {
-    if(e.key === 'F11') ipcRenderer.send('fullscreen');
-    if((e.metaKey || e.ctrlKey) && e.key === '+') ipcRenderer.send('zoomIn');
-    if((e.metaKey || e.ctrlKey) && e.key === '-') ipcRenderer.send('zoomOut');
-    if((e.metaKey || e.ctrlKey) && e.key === '=') ipcRenderer.send('zoomReset');
-    if(e.key === 'F5' || ((e.metaKey || e.ctrlKey) && (e.key === 'R' || e.key === 'r'))) ipcRenderer.send('reloadView');
+    if (e.key === 'F11') ipcRenderer.send('fullscreen');
+    if ((e.metaKey || e.ctrlKey) && e.key === '+') ipcRenderer.send('zoomIn');
+    if ((e.metaKey || e.ctrlKey) && e.key === '-') ipcRenderer.send('zoomOut');
+    if ((e.metaKey || e.ctrlKey) && e.key === '=') ipcRenderer.send('zoomReset');
+    if (e.key === 'F5' || ((e.metaKey || e.ctrlKey) && (e.key === 'R' || e.key === 'r'))) ipcRenderer.send('reloadView');
 });
 
 window.addEventListener('wheel', (e) => {
-   if(e.ctrlKey || e.metaKey) {
-       if (e.deltaY < 0) {
-           ipcRenderer.send('zoomIn');
-       } else {
-           ipcRenderer.send('zoomOut');
-       }
-   }
+    if (e.ctrlKey || e.metaKey) {
+        if (e.deltaY < 0) {
+            ipcRenderer.send('zoomIn');
+        } else {
+            ipcRenderer.send('zoomOut');
+        }
+    }
 });
